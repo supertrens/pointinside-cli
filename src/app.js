318f819184d584518e341c53d073bd7d47cbc4e5
Ernst Pierre <pitrens.studio@gmail.com>
2021-03-04 18:00:22 +0800

    code: showing app options

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 import boxen from "boxen";
+import inquirer from "inquirer";
+
 import { logger } from "./utils/helper.js";
+import { BY_CITY, BY_LAT_LNG, MENU_LIST } from "./utils/constant.js";
+
+function init() {
+  greeting();
+  loadMenu();
+}
 
 function greeting() {
   const greeting = "Welcome to the pointinside Node / Backend Coding Exercise";
@@ -14,4 +22,20 @@ function greeting() {
   logger(boxen(greeting, boxenOptions));
 }
 
-greeting();
+async function loadMenu() {
+  const { menu } = await inquirer.prompt(MENU_LIST);
+  switch (menu) {
+    case BY_CITY:
+      console.log("Look for city");
+      // TODO: inquiryByCity()
+      break;
+    case BY_LAT_LNG:
+      console.log("Look for lat and lng");
+      // TODO: inquiryByLatAndLng()
+      break;
+    default:
+      await inquiryLatLng();
+  }
+}
+
+init();
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,4 +4,19 @@ const ERROR_COLOR = "red";
 const apiKey = process.env.TIME_ZONE_DB_API_KEY || "YP1WD9YX00FW";
 const BASE_URL = `http://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}`;
 
-export { SUCCESS_COLOR, ERROR_COLOR, BASE_URL };
+const BY_CITY = "City";
+const BY_LAT_LNG = "Latitude and Longitude";
+
+/** Menu */
+const MENU_LIST = [
+  {
+    type: "rawlist",
+    name: "menu",
+    message:
+      "Do you want to lookup a timezone by City or Lattitude/Longitude (default)",
+    choices: [BY_CITY, BY_LAT_LNG],
+    default: "Lat & Lng",
+  },
+];
+
+export { SUCCESS_COLOR, ERROR_COLOR, BASE_URL, MENU_LIST, BY_CITY, BY_LAT_LNG };
